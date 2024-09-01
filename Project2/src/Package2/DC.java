9af54292a4ca584190b0d34e49571be7d1cf1fbc
Sekharkati <k.r.sekhar.1996@gmail.com>
2024-09-01 09:26:12 +0530

    has a relationship creating object of another class in a different project

---
+++ Project2/src/Package2/DC.java
@@ -0,0 +1,12 @@
+package Package2;
+
+import package1.Student;
+
+public class DC {
+	public static void main(String[] args) {
+		Student ob=new Student();
+		ob.display1();
+		ob.display2();
+	}
+
+}
